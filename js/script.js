8dfb5fb659a1248976aa980fa14cb1b6970e0036
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2024-03-18 14:34:58 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const category_button = document.getElementById("category_button")
 const category = document.getElementById("category")
+const products = document.getElementById("subwrapper")
 
 function show_categories() {
     category.classList.toggle('category_visible')
+    products.classList.toggle('products_visible')
 }
 
 category_button.addEventListener('click', show_categories)
